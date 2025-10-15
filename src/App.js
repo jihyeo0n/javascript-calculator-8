4bba7f64f6bc1854734e9cb2b14c1f90b6b4d700
현지현 <qwere4585@naver.com>
2025-10-15 16:57:32 +0900

    feat: 모든 숫자의 합 계산 추가

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ class App {
 
     if (input === "") result = 0;
     if (input.includes(",") || input.includes(":")) {
-      result = input.split(/,|:/).join("");
+      result = input.split(/,|:/).map(Number).reduce((acc,cur) => acc + cur, 0);
     }
     if(input.startsWith("//") && input.includes("\\n")) {
       const [delimiterPart, numbersPart] = input.split("\\n");
       const customDelimiter = delimiterPart.slice(2);
-      const numbers = numbersPart.split(customDelimiter).map(Number);
-      result = numbers.join("")
+      const numbers = numbersPart.split(customDelimiter).map(Number).reduce((acc,cur) => acc + cur, 0);
+      result = numbers;
     }
 
     MissionUtils.Console.print(`결과 : ${result}`);
