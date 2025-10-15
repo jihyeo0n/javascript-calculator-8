797ec49221242cdd5b68cbd7bb572ce391e8bf43
현지현 <qwere4585@naver.com>
2025-10-15 16:07:24 +0900

    feat: 빈 문자열 입력 시 0 반환 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요. \n");
+    let result = '';
+
+    if(input === "") result = 0;
+
+    MissionUtils.Console.print(`결과 : ${result}`);
   }
 }
 
