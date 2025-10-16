f1a78e17cf962bc45d7a69906a99566d814a7c1d
현지현 <qwere4585@naver.com>
2025-10-16 14:48:30 +0900

    feat: 예외 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,24 +2,43 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await MissionUtils.Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요. \n"
-    );
-    let result = "";
+    try {
+      const input = await MissionUtils.Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요. \n"
+      );
+      let result = "";
 
-    if (input === "") result = 0;
-    else if (input.includes(",") || input.includes(":")) {
-      result = input.split(/,|:/).map(Number).reduce((acc,cur) => acc + cur, 0);
-    }
-    else if(input.startsWith("//") && input.includes("\\n")) {
-      const [delimiterPart, numbersPart] = input.split("\\n");
-      const customDelimiter = delimiterPart.slice(2);
-      const numbers = numbersPart.split(customDelimiter).map(Number).reduce((acc,cur) => acc + cur, 0);
-      result = numbers;
-    }
-    else result = Number(input);
+      if (input === "") result = 0;
+      else if (input.includes(",") || input.includes(":")) {
+        const numbers = input.split(/,|:/).map(Number);
 
-    MissionUtils.Console.print(`결과 : ${result}`);
+        this.validateNumbers(numbers);
+        result = numbers.reduce((acc, cur) => acc + cur, 0);
+      } else if (input.startsWith("//") && input.includes("\\n")) {
+        const [delimiterPart, numbersPart] = input.split("\\n");
+        const customDelimiter = delimiterPart.slice(2);
+        const numbers = numbersPart.split(customDelimiter).map(Number);
+
+        this.validateNumbers(numbers);
+        result = numbers.reduce((acc, cur) => acc + cur, 0);
+      } else {
+        const num = Number(input);
+        this.validateNumbers([num]);
+        result = num;
+      }
+
+      MissionUtils.Console.print(`결과 : ${result}`);
+    } catch (error) {
+      MissionUtils.Console.print(`[ERROR] ${error.message}`);
+    }
+  }
+
+  validateNumbers(numbers) {
+    numbers.forEach((num) => {
+      if (isNaN(num) || num < 0) {
+        throw new Error("잘못된 입력입니다. 양수만 입력해 주세요.");
+      }
+    });
   }
 }
 
